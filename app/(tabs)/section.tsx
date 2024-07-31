1b629cfe314b25a403c4be0a296340bb6aefcc02
rxur <armando_rour@outlook.com>
2024-07-31 12:16:25 -0600

    This commit is a test

---
+++ app/(tabs)/section.tsx
@@ -0,0 +1,39 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Image, Platform } from "react-native";
+
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
+      headerImage={
+        <Ionicons size={310} name="code-slash" style={styles.headerImage} />
+      }
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">¡Hola!</ThemedText>
+      </ThemedView>
+      <ThemedText>
+        Esta es una prueba de que funciona el proyecto correctamente.
+      </ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: "#808080",
+    bottom: -90,
+    left: -35,
+    position: "absolute",
+  },
+  titleContainer: {
+    flexDirection: "row",
+    gap: 8,
+  },
+});
