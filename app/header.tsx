45faff3fbef016a35a71e6ea86d6b7bcb0b08e38
rxur <armandorour@outlook.com>
2024-08-12 12:52:02 -0600

    Update Header component and logo

--- app/header.tsx
+++ app/header.tsx
@@ -1,13 +1,34 @@
 import * as React from "react";
 import { Appbar } from "react-native-paper";
+import { Image, View } from "react-native";
+import { useGlobalStyles } from "@/styles/globalStyles";
+import { Text, Divider, Icon } from "react-native-paper";
+import { AppTheme, useAppTheme } from "@/app/_layout";
 
 const Header = () => {
-  const _goBack = () => console.log("Went back");
+  const styles = useGlobalStyles();
+  const theme: AppTheme = useAppTheme();
 
   return (
-    <Appbar.Header>
-      <Appbar.BackAction onPress={_goBack} />
-      <Appbar.Content title="Oxito" />
+    <Appbar.Header
+      style={{
+        backgroundColor: theme.colors.brandTertiary,
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingHorizontal: 16,
+      }}
+    >
+      <Image
+        source={require("@/assets/images/oxbi-logo.png")}
+        style={{ width: 56.667, height: 34 }}
+      />
+
+      <View style={{ flexDirection: "row", gap: 8 }}>
+        <Icon source="account-circle" size={24} color="white" />
+        <Text style={{ color: "white", fontSize: 16, fontFamily: "Work-Sans" }}>
+          Mario
+        </Text>
+      </View>
     </Appbar.Header>
   );
 };
